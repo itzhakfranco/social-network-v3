d0cb949d3b8a51326799684efbd10d8a2177221f
Itzhak Franco <itzhakfranco@gmail.com>
2021-04-13 06:34:22 +0300

    added persisted storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import Navbar from "./components/layout/navbar/navbar.jsx";
 import { Route, Switch } from "react-router-dom";
 
+import ProtectedRoute from "./components/common/protectedRoute";
+import Signin from "./components/forms/auth/signin";
+import Signup from "./components/forms/auth/signup";
+import Dashboard from "./components/pages/Dashbord/dashboard";
+
+import ExperienceForm from "./components/forms/profile/experience-form";
+import ProfileForm from "./components/forms/profile/profile-form";
+
 import Routes from "./routing/routes";
 
 function App() {
@@ -11,8 +19,15 @@ function App() {
 			</header>
 			<main style={{ minHeight: "900px" }}>
 				<Switch>
-					<Route exact path='/' />
-					<Route component={Routes} />
+					<Route exact path='/user/signup' component={Signup} />
+					<Route exact path='/user/signin' component={Signin} />
+					<Route exact path='/user/create-profile' component={ProfileForm} />
+					<Route exact path='/user/dashboard' component={Dashboard} />
+					<Route
+						exact
+						path='/user/profile/experience'
+						component={ExperienceForm}
+					/>
 				</Switch>
 			</main>
 			<footer></footer>
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -13,9 +13,33 @@ const rootReducer = combineReducers({
 	experiences: experienceReducer,
 });
 
+const loadState = () => {
+	try {
+		const serializedState = localStorage.getItem("state");
+		if (serializedState === null) {
+			return undefined;
+		}
+		return JSON.parse(serializedState);
+	} catch (e) {
+		return undefined;
+	}
+};
+
+const saveState = (state) => {
+	const serializedState = JSON.stringify(state);
+	localStorage.setItem("state", serializedState);
+};
+
+const peristedState = loadState();
+
 const store = createStore(
 	rootReducer,
+	peristedState,
 	composeEnhancers(applyMiddleware(thunk))
 );
+
+store.subscribe(() => {
+	saveState(store.getState());
+});
 
 export default store;

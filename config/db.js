2302c967cde7ba7423a39ea27f138fbffd0986b3
Itzhak Franco <itzhakfranco@gmail.com>
2021-04-27 22:34:24 +0300

    preparing for deployment

--- config/db.js
+++ config/db.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect("mongodb://localhost:27017/myapp", {
-			useNewUrlParser: true,
-			useFindAndModify: false,
-			useCreateIndex: true,
-			useUnifiedTopology: true,
-		});
+		await mongoose.connect(
+			process.env.MONGODB_URI || "mongodb://localhost:27017/myapp",
+			{
+				useNewUrlParser: true,
+				useFindAndModify: false,
+				useCreateIndex: true,
+				useUnifiedTopology: true,
+			}
+		);
 		console.log("MongoDB Connected");
 	} catch (err) {
 		console.error("Could not connect to MongoDB...");

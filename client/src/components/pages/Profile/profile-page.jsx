085176981e25cdad92410f701c47fbfcec5dddcf
itzhakfranco <62960587+itzhakfranco@users.noreply.github.com>
2021-04-28 20:20:48 +0300

    Create profile-page.jsx

---
+++ client/src/components/pages/Profile/profile-page.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import {
+	fetchProfileById,
+	fetchUserProfile,
+} from "../../../store/profile/profileActions";
+import PreLoader from "../../../utils/pre-loader";
+import ProfileHeader from "./profile-header";
+import ProfilleActions from "./profile-actions";
+import ExperienceSection from "./experience-section";
+import EducationSection from "./education-section";
+
+const GuestProfilePage = ({
+	fetchProfileById,
+	fetchUserProfile,
+	match,
+	profile,
+	loading,
+	user_id,
+}) => {
+	useEffect(() => {
+		if (match.params.id) {
+			fetchProfileById(match.params.id);
+		} else {
+			fetchUserProfile();
+		}
+	}, [match.params.id]);
+
+	return loading ? (
+		<PreLoader />
+	) : (
+		<div className='container'>
+			<ProfilleActions profile={profile} user_id={user_id} />
+			<ProfileHeader profile={profile} />
+			<ExperienceSection experience={profile?.experience} />
+			<EducationSection education={profile?.education} />
+		</div>
+	);
+};
+
+const mapStateToProps = (state) => ({
+	profile: state.profile.guestProfile,
+	loading: state.profile.loading,
+	user_id: state.user.user_id,
+});
+
+export default connect(mapStateToProps, { fetchProfileById, fetchUserProfile })(
+	GuestProfilePage
+);

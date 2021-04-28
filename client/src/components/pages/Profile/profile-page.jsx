dc95d33e4bd899c6eb6afbf5fe65a822aa50cccf
itzhakfranco <62960587+itzhakfranco@users.noreply.github.com>
2021-04-28 20:55:49 +0300

    Update profile-page.jsx

--- client/src/components/pages/Profile/profile-page.jsx
+++ client/src/components/pages/Profile/profile-page.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
 	fetchProfileById,
@@ -10,33 +10,35 @@ import ProfilleActions from "./profile-actions";
 import ExperienceSection from "./experience-section";
 import EducationSection from "./education-section";
 
-const GuestProfilePage = ({
-	fetchProfileById,
-	fetchUserProfile,
-	match,
-	profile,
-	loading,
-	user_id,
-}) => {
-	useEffect(() => {
-		if (match.params.id) {
-			fetchProfileById(match.params.id);
+class PofilePage extends Component {
+	componentDidMount() {
+		if (this.props.params.id) {
+			this.props.fetchProfileById(this.props.match.params.id);
 		} else {
-			fetchUserProfile();
+			this.props.fetchUserProfile();
 		}
-	}, [match.params.id,fetchProfileById,fetchUserProfile]);
+	}
 
-	return loading ? (
-		<PreLoader />
-	) : (
-		<div className='container'>
-			<ProfilleActions profile={profile} user_id={user_id} />
-			<ProfileHeader profile={profile} />
-			<ExperienceSection experience={profile?.experience} />
-			<EducationSection education={profile?.education} />
-		</div>
-	);
-};
+	componentDidUpdate(previousProps) {
+		if (previousProps.match.params.id !== this.props.match.params.id) {
+			this.props.fetchProfileById(this.props.match.params.id);
+		}
+	}
+	render() {
+		const { profile, loading, user_id } = this.props;
+
+		return loading ? (
+			<PreLoader />
+		) : (
+			<div className='container'>
+				<ProfilleActions profile={profile} user_id={user_id} />
+				<ProfileHeader profile={profile} />
+				<ExperienceSection experience={profile?.experience} />
+				<EducationSection education={profile?.education} />
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = (state) => ({
 	profile: state.profile.guestProfile,
@@ -45,5 +47,6 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, { fetchProfileById, fetchUserProfile })(
-	GuestProfilePage
+	PofilePage
 );
+

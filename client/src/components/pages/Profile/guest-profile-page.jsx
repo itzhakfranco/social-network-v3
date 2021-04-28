ba65145c0c93e8f51f5dafaa929f073df16986dc
itzhakfranco <62960587+itzhakfranco@users.noreply.github.com>
2021-04-28 19:12:04 +0300

    Update guest-profile-page.jsx

--- client/src/components/pages/Profile/guest-profile-page.jsx
+++ client/src/components/pages/Profile/guest-profile-page.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchProfileById } from "../../../store/profile/profileActions";
 import PreLoader from "../../../utils/pre-loader";
@@ -6,37 +6,33 @@ import ProfileHeader from "./profile-header";
 import ProfilleActions from "./profile-actions";
 import ExperienceSection from "./experience-section";
 import EducationSection from "./education-section";
+const GuestProfilePage = ({
+	fetchProfileById,
+	match,
+	profile,
+	loading,
+	user_id,
+}) => {
+	useEffect(() => {
+		fetchProfileById(match.params.id);
+	}, [match.params.id]);
 
-class GuestProfilePage extends Component {
-    componentDidMount() {
-        this.props.fetchProfileById(this.props.match.params.id);
-    }
-
-    componentDidUpdate(previousProps) {
-        if (previousProps.match.params.id !== this.props.match.params.id) {
-            this.props.fetchProfileById(this.props.match.params.id);
-        }
-    }
-    render() {
-        const { profile, loading, user_id } = this.props;
-
-        return loading ? (
-            <PreLoader />
-        ) : (
-            <div className='container'>
-                <ProfilleActions profile={profile} user_id={user_id} />
-                <ProfileHeader profile={profile} />
-                <ExperienceSection experience={profile?.experience} />
-                <EducationSection education={profile?.education} />
-            </div>
-        );
-    }
-}
+	return loading ? (
+		<PreLoader />
+	) : (
+		<div className='container'>
+			<ProfilleActions profile={profile} user_id={user_id} />
+			<ProfileHeader profile={profile} />
+			<ExperienceSection experience={profile?.experience} />
+			<EducationSection education={profile?.education} />
+		</div>
+	);
+};
 
 const mapStateToProps = (state) => ({
-    profile: state.profile.guestProfile,
-    loading: state.profile.loading,
-    user_id: state.user.user_id,
+	profile: state.profile.guestProfile,
+	loading: state.profile.loading,
+	user_id: state.user.user_id,
 });
 
 export default connect(mapStateToProps, { fetchProfileById })(GuestProfilePage);
